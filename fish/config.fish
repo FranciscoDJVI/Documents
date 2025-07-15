21869872d22d8ad52cccd2b810dfd7b58bdf70d3
Francisco Vanegas <vanegasfrancisco415@gmail.com>
2025-07-15 12:15:29 -0500

    config tmux, fish

---
+++ fish/config.fish
@@ -0,0 +1,52 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias ls "ls -p -G"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+alias c claude
+command -qv nvim && alias vim nvim
+function l
+    command eza -l -i --icons --colour $argv
+end
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+# NodeJS
+set -gx PATH node_modules/.bin $PATH
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
+switch (uname)
+    case Darwin
+        source (dirname (status --current-filename))/config-osx.fish
+    case Linux
+        source (dirname (status --current-filename))/config-linux.fish
+    case '*'
+        source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+    source $LOCAL_CONFIG
+end
+set -g fish_user_paths /usr/local/bin $fish_user_paths
+set -g fish_user_paths /usr/local/sbin $fish_user_paths
+eval (oh-my-posh init fish)
+eval "$(oh-my-posh init fish --config /usr/share/oh-my-posh/themes/spaceship.omp.json)"
---
+++ fish/functions/config-linux.fish
@@ -0,0 +1,4 @@
+if type -q eza
+    alias ll "eza -l -g --icons --colour"
+    alias lla "ll -a"
+end
